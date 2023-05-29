e2015afcb03b4c8b1f484a755e7793248f22001b
Amil Ali <66510886+amilali@users.noreply.github.com>
2023-05-29 18:08:28 +0530

    Add files via upload

---
+++ web_editor/script.js
@@ -0,0 +1,37 @@
+document.onreadystatechange = function() {
+  if (document.readyState !== "complete") {
+      document.querySelector(
+        "body").style.visibility = "hidden";
+      document.querySelector(
+        "#loader").style.visibility = "visible";
+  } else {
+      document.querySelector(
+        "#loader").style.display = "none";
+      document.querySelector(
+        "body").style.visibility = "visible";
+  }
+};
+
+function compile() {
+    var html = document.getElementById("html");
+    var css = document.getElementById("css");
+    var js = document.getElementById("js");
+    var code = document.getElementById("code").contentWindow.document;
+  
+    document.body.onkeyup = function() {
+      code.open();
+      code.writeln(
+        html.value +
+          "<style>" +
+          css.value +
+          "</style>" +
+          "<script>" +
+          js.value +
+          "</script>"
+      );
+      code.close();
+    };
+  }
+  
+  compile();
+
